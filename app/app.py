f6df7db1b3e1bd6ea4bde9b3eb1bf71bdd9d7716
Sri13-13 <balasridurga915@gmail.com>
2024-09-26 12:59:07 +0530

    third commit

--- app/app.py
+++ app/app.py
@@ -1,20 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 #initialize flask
 app=Flask(__name__)
 
+#database configuration
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:admin123@localhost/student_management'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+#initialize the database
+db=SQLAlchemy(app)
+
+#define the student model
+class Students(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(100),nullable=False)
+    age=db.Column(db.Integer,nullable=False)
+    course=db.Column(db.String(100),nullable=False)
+
+    def __repr__(self):
+        return f"Student({self.name},{self.age},{self.course})"
+
 #define routes
+#root for home page to display the list of students
 @app.route('/')
 def home():
-    return render_template('index.html')
+    std=Students.query.all()  #fetch all student records from the database
+    return render_template('index.html',students=std)
 
-@app.route('/profile')
-def profile():
-    return render_template('profile.html')
+@app.route('/add',methods=['POST'])
+def add():
+    formName= request.form['name']
+    formAge= request.form['age']
+    formCourse= request.form['course']
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+    #create new student record
+    newStudent=Students(name=formName, age=formAge, course= formCourse)
+    db.session.add(newStudent)
+
+    db.session.commit()
+    return redirect(url_for('home'))
+
+@app.route('/edit/<int:id>',methods=['POST','GET'])
+def edit_student(id):
+    std= Students.query.get_or_404(id)  # get the student by id
+    if request.method=="POST":
+        std.name=request.form['name']
+        std.age=request.form['age']
+        std.course=request.form['course']
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('edit.html',student=std)
 
 if __name__=='__main__':
     app.run(debug=True)
